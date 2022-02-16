89aed2fbf54c58c9d8f9d647d528d592d7fb3ce2
Louis CORD'HOMME <louis.cordhomme@gmail.com>
2022-02-16 23:34:19 +0100

    support video

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -6,13 +6,23 @@ function mediaFactory(
     const path = name.split(' ')[0].split('-').join(' ')
     const figure = document.createElement('figure')
     figure.classList.add('media_card')
+    figure.classList.toggle('video', !!video)
     figure.innerHTML = `
-      <img src=assets/images/${path}/${image || video} alt='${title}'>
+      ${
+        image
+          ? `<img src=assets/images/${path}/${image} alt='${title}'>`
+          : `
+          <video>
+            <source src=assets/images/${path}/${video} type='video/mp4'>
+            <p>Votre navigateur ne supporte pas les vidéos HTML5.</p>
+          </video>
+        `
+      }
       <figcaption>
         <h2>${title}</h2>
         <span>
           <p>${likes}</p>
-          <i class="fas fa-heart"></i>
+          <i class="fas fa-heart" aria-label='likes' ></i>
         </span>
       </figcaption>
     `
